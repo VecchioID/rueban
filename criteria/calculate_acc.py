de6c3e920d8961784c373505ca9329ecd62bf038
VecchioID <zhaokaizhuce@foxmail.com>
2022-12-29 13:07:02 +0800

    init

--- criteria/calculate_acc.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-# __author__ = 'Vecchio'
-
-def calculate_acc(output, target):
-    pred = output.data.max(1)[1]
-    correct = pred.eq(target.data).cpu().sum().numpy()
-    return correct * 100.0 / target.size()[0]
